71fcb24ac4c930d17af463895943eb8ac7e3ac08
Pásztor János <model87@freemail.hu>
2011-06-30 10:27:45 +0200

    enabled more disabled commands

--- init.el
+++ init.el
@@ -3,6 +3,8 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
 
 ;; Proper start
 
