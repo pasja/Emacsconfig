32c157fdd2ab0fe0da57621a62277f80075a262b
Pásztor János <model87@freemail.hu>
2012-06-06 11:50:33 +0200

    Added easy timestamp insert for rovancs.org

--- init.el
+++ init.el
@@ -219,10 +219,15 @@
       org-return-follows-link t
       org-CUA-compatible t)
 
-(add-hook 'org-shiftup-final-hook 'windmove-up)        ; Make windmove work in org-mode
+(add-hook 'org-shiftup-final-hook 'windmove-up)         ; Make windmove work in org-mode
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
+
+(global-set-key (kbd "M-3")                             ; easy timestamp for rovancs.org
+		(lambda ()
+		  (interactive)
+		  (insert(format-time-string "%Y-%m-%d"))))
 
 ;; configure dired
 
