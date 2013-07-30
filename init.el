322e4e1a8558a812c57bb54378f8e66cff2f55d9
Pásztor János <model87@freemail.hu>
2013-07-30 16:39:04 +0200

    Restores the previous window configuration and kills the magit buffer

--- init.el
+++ init.el
@@ -406,8 +406,21 @@
 			(global-set-key (kbd "C-x g")
 					(lambda ()
 					  (interactive)
-					  (magit-status default-directory)
-					  (delete-other-windows)))))
+					  (magit-status default-directory)))
+
+			(defadvice magit-status (around magit-fullscreen activate) ; full screen magit-status
+			  (window-configuration-to-register :magit-fullscreen)
+			  ad-do-it
+			  (delete-other-windows))
+
+			(defun magit-quit-session ()
+			  "Restores the previous window configuration and kills the magit buffer"
+			  (interactive)
+			  (kill-buffer)
+			  (jump-to-register :magit-fullscreen))
+
+			(eval-after-load 'magit
+			  '(define-key magit-status-mode-map (kbd "q") 'magit-quit-session))))
 
 	(:name dired+
 	       :after (progn
