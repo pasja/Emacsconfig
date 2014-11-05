a0c1bcfae32f2ac3a70226c4788f9d0799f3c1eb
Pásztor János <model87@freemail.hu>
2014-11-05 15:09:16 +0100

    Haskell-mode sandbox config

--- init.el
+++ init.el
@@ -593,7 +593,9 @@
 	       :build ("make all")
 	       :post-init (progn
 			    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-			    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)))
+			    (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+			    (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+			    (setq haskell-process-type 'cabal-repl)))
 
 	(:name org-mode
 	       :website "http://orgmode.org/"
