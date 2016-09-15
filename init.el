33b9fe9d550a5d962d5816f2af3c084e2e0eb26f
Pásztor János <janos.pasztor@balabit.com>
2016-09-15 15:59:37 +0200

    Start using more from the ivy ecosystem

--- init.el
+++ init.el
@@ -521,6 +521,14 @@
 
 (el-get-bundle swiper
   (ivy-mode 1)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+
   (defun ivy-immediate-done () ; temporary override
     "Exit the minibuffer with current input instead of current candidate."
     (interactive)
