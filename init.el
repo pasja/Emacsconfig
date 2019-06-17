51ec3e210137c9370fa97781809fe5860f86e557
Pásztor János <janos.pasztor@balabit.com>
2019-06-17 14:26:25 +0200

    Further fine-tune for org-mode

--- init.el
+++ init.el
@@ -383,7 +383,9 @@
           org-CUA-compatible t
           org-fontify-done-headline t
           org-highlight-latex-and-related '(latex)
-          org-M-RET-may-split-line '((default . nil)))
+          org-M-RET-may-split-line '((default . nil))
+          org-agenda-start-on-weekday 1
+          org-enforce-todo-checkbox-dependencies t)
 
     (add-hook 'org-shiftup-final-hook 'windmove-up)         ; Make windmove work in org-mode
     (add-hook 'org-shiftleft-final-hook 'windmove-left)
