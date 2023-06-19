ce47d0d91a97f5a9dca858378c24c35f2c88fa9f
János Pásztor <model87@freemail.hu>
2023-06-19 15:15:58 +0200

    Second attempt of fixing the byte-compilation stuff

--- init.el
+++ init.el
@@ -355,12 +355,12 @@
 
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil t)
-  (url-retrieve
-   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (_s)
-     (goto-char (point-max))
-     (eval-print-last-sexp))))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
 ;; configure elpa
 
@@ -672,6 +672,8 @@
         (require 'ercidentities)
         (enable-circe-new-day-notifier)
         (add-to-list 'circe-format-not-tracked 'circe-new-day-notifier-format-message)))))
+
+(el-get 'sync)
 
 ;; External libraries
 
