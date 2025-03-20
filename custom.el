45a328addf40bffb3f333e3fbf597f67ff3dc06f
János Pásztor <model87@freemail.hu>
2025-03-20 14:45:58 +0100

    compat is usually a dependency of magit

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(frame-background-mode 'dark)
- '(package-selected-packages '(let-alist)))
+ '(package-selected-packages '(compat let-alist)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
